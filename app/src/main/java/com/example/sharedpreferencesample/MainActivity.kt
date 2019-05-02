35351dca737440c8ab57aad1c633611785888b31
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-02 22:04:27 +0900

    SharedPreferenceのデータを表示、保存する処理を実装

--- app/src/main/java/com/example/sharedpreferencesample/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesample/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.sharedpreferencesample
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pref = getSharedPreferences("file_name", Context.MODE_PRIVATE)
+
+        val storedText = pref.getString("key", getString(R.string.notStored))
+
+        editText.setText(storedText)
+
+        store.setOnClickListener {
+            val inputText = editText.text.toString()
+            pref.edit().putString("key", inputText).apply()
+        }
     }
 }
